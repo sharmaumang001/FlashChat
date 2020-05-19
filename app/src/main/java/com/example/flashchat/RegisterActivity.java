acfb59fad902ae8b8e9aa981def20490534f77e4
umang <iumangsharma001@gmail.com>
2020-05-20 01:48:06 +0530

    2.activities created.

---
+++ app/src/main/java/com/example/flashchat/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.example.flashchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
