92d39af043a0f82464d445e9a0b597f6693ecdd6
umang <iumangsharma001@gmail.com>
2020-05-20 03:50:17 +0530

    Both Activities Fully linked .

--- app/src/main/java/com/example/flashchat/MainActivity.java
+++ app/src/main/java/com/example/flashchat/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.flashchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEmailView,mPasswordView;
+    Button mSignInButton,mRegisterButton;
 
 
 
@@ -12,5 +19,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmailView = findViewById(R.id.emailId);
+        mPasswordView = findViewById(R.id.passwordID);
+        mSignInButton = findViewById(R.id.signInID);
+        mRegisterButton= findViewById(R.id.registerID);
+
     }
+
+    public void signInExistingUser(View V ){
+
+    }
+
+    public void registerNewUser (View V ){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/flashchat/RegisterActivity.java
+++ app/src/main/java/com/example/flashchat/RegisterActivity.java
@@ -2,12 +2,81 @@ package com.example.flashchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    AutoCompleteTextView mRegiterEmailView,mRegisterUserNameView;
+    EditText mRegiterPasswordView,mConfirmPasswordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mRegisterUserNameView = findViewById(R.id.usernameId);
+        mRegiterEmailView= findViewById(R.id.emailRegisterId);
+        mRegiterPasswordView= findViewById(R.id.passwordRegisterId);
+        mConfirmPasswordView = findViewById(R.id.passwordConfirmId);
+
     }
+
+    public void SignUp (View V){
+        attemptRegistration();
+    }
+
+    public void attemptRegistration(){
+
+        mRegiterEmailView.setError(null);
+        mRegiterPasswordView.setError(null);
+
+        String mEmail = mRegiterEmailView.getText().toString();
+        String mPassword = mRegiterPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View mfocusView= null;
+
+        if(!TextUtils.isEmpty(mPassword) && (!isPasswordValid(mPassword))){
+            mRegiterPasswordView.setError("Password is too short or does not match");
+            mfocusView = mRegiterPasswordView;
+            cancel = true;
+        }
+        if(!TextUtils.isEmpty(mEmail)){
+            mRegiterEmailView.setError("The Field is reuired");
+            mfocusView = mRegiterEmailView;
+            cancel= true;
+        }else if(!isEmailValid(mEmail)){
+            mRegiterEmailView.setError("The Email is invalid ");
+            mfocusView = mRegiterEmailView;
+            cancel= true;
+        }
+
+        if(cancel){
+            mfocusView.requestFocus();
+        }else{
+
+        }
+    }
+
+    public boolean isEmailValid(String email){
+
+        return email.contains("@");
+    }
+    public boolean isPasswordValid(String password){
+
+        return true;
+    }
+
+
+
+
+
+
+
 }
