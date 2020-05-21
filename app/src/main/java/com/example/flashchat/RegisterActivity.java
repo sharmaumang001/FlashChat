09d70f502a18da7fe3a2b508bf311bd28beb1db7
umang <iumangsharma001@gmail.com>
2020-05-21 12:20:54 +0530

    Registration Process Done.

--- app/src/main/java/com/example/flashchat/RegisterActivity.java
+++ app/src/main/java/com/example/flashchat/RegisterActivity.java
@@ -1,19 +1,27 @@
 package com.example.flashchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
 
-    AutoCompleteTextView mRegiterEmailView,mRegisterUserNameView;
-    EditText mRegiterPasswordView,mConfirmPasswordView;
+    public AutoCompleteTextView mRegisterEmailView,mRegisterUserNameView;
+    public EditText mRegiterPasswordView,mConfirmPasswordView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +29,11 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mRegisterUserNameView = findViewById(R.id.usernameId);
-        mRegiterEmailView= findViewById(R.id.emailRegisterId);
+        mRegisterEmailView= findViewById(R.id.emailRegisterId);
         mRegiterPasswordView= findViewById(R.id.passwordRegisterId);
         mConfirmPasswordView = findViewById(R.id.passwordConfirmId);
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -31,13 +41,16 @@ public class RegisterActivity extends AppCompatActivity {
         attemptRegistration();
     }
 
+
     public void attemptRegistration(){
 
-        mRegiterEmailView.setError(null);
+        mRegisterEmailView.setError(null);
         mRegiterPasswordView.setError(null);
 
-        String mEmail = mRegiterEmailView.getText().toString();
+        String mEmail = mRegisterEmailView.getText().toString();
         String mPassword = mRegiterPasswordView.getText().toString();
+
+        Log.d("flashchat","The Email is "+mEmail);
 
         boolean cancel = false;
         View mfocusView= null;
@@ -47,30 +60,53 @@ public class RegisterActivity extends AppCompatActivity {
             mfocusView = mRegiterPasswordView;
             cancel = true;
         }
-        if(!TextUtils.isEmpty(mEmail)){
-            mRegiterEmailView.setError("The Field is required");
-            mfocusView = mRegiterEmailView;
+        if(TextUtils.isEmpty(mEmail)){
+            mRegisterEmailView.setError("The Field is required");
+            mfocusView = mRegisterEmailView;
             cancel= true;
         }else if(!isEmailValid(mEmail)){
-            mRegiterEmailView.setError("The Email is invalid ");
-            mfocusView = mRegiterEmailView;
+            mRegisterEmailView.setError("The Email is invalid ");
+            mfocusView = mRegisterEmailView;
             cancel= true;
         }
 
         if(cancel){
             mfocusView.requestFocus();
         }else{
-
+            createFireBaseUser();
         }
     }
+
 
     public boolean isEmailValid(String email){
 
         return email.contains("@");
     }
-    public boolean isPasswordValid(String password){
 
-        return true;
+
+    public boolean isPasswordValid(String password){
+       String mcomfirmedPassword =  mConfirmPasswordView.getText().toString();
+       return mcomfirmedPassword.equals(password) && password.length()>7;
+    }
+
+
+    public void createFireBaseUser(){
+
+        String email = mRegisterEmailView.getText().toString();
+        String password = mRegiterPasswordView.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("flashchat","Create user Completed"+task.isSuccessful());
+
+                if(!task.isSuccessful()){
+                    Log.d("flashchat","Create user Not Complted ");
+                }
+
+            }
+        });
+
     }
 
 
